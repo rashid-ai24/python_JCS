5639b8e5da2bb31778501799309fb74eaa15d2d5
MOHD_RASHID <moh.rashid20006@gmail.com>
2025-08-30 21:25:03 +0530

    Improve division error handling and loop structure

--- function_pgrm.py
+++ function_pgrm.py
@@ -22,15 +22,22 @@ def calc(a):
             print("Division of two numbers")
             a=int(input("Enter the first number: "))
             b=int(input("Enter the second number: "))
-            c=a/b
-            print(f"The division of the two numbers is {c}")
-
+            try: 
+                c=a/b
+                print(f"The division of the two numbers is {c}")
+            except ZeroDivisionError:
+                print("Can't divide by zero:")
         case _:
             print("invalid input")
 
-
-print("This is a calculation program")
-print("1.Addition, 2.Subtraction, 3.Multiplication, 4.Division")
-a=int(input("Enter your choice: "))
-calc(a)
-    
+while True:
+    print("This is a calculation program")
+    print("0.Exit, 1.Addition, 2.Subtraction, 3.Multiplication, 4.Division")
+    a=int(input("Enter your choice: "))
+    if a==1 or a==2 or a==3 or a==4:
+        calc(a)
+    elif a==0:
+        print("Exiting program:")
+        break
+    else:
+        print("Invalid choice.")
